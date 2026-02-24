892e48a77470bbd84d68739c994351a7e285a18d
dinihhh <hudini@ioz.ac.cn>
2026-02-24 14:07:00 +0800

    Update meta.sh

--- meta.sh
+++ meta.sh
@@ -77,6 +77,32 @@ time metabat2 -m 1500 -t 16 -i Result/Megahit/final.contigs.fa -a Result/MAG/sam
 #Checkm
 mkdir Result/MAG/checkm
 checkm lineage_wf -t 16 -x fa Result/MAG/sample_binning/sample/ Result/MAG/checkm
+checkm qa \
+    Result/MAG/checkm/lineage.ms \
+    Result/MAG/checkm \
+    -o 2 \
+    > Result/MAG/checkm/checkm_summary.tsv
+    awk 'BEGIN{OFS=","} NR==1{print "genome,completeness,contamination"} NR>1{print $1,$12,$13}' \
+Result/MAG/checkm/checkm_summary.tsv \
+> Result/MAG/checkm/genomeInfo.csv
+#dRep
+dRep dereplicate Result/MAG/drep_out \
+    -g Result/MAG/sample_binning/sample/*.fa \
+    --genomeInfo Result/MAG/checkm/genomeInfo.csv \
+    -p 32 \
+    --completeness 90 \
+    --contamination 5 \
+    --S_algorithm fastANI \
+    -sa 0.95
+#CoverM
+coverm genome \
+  -d Result/MAG/drep_out/dereplicated_genomes \
+  -1 Result/${SAMPLE}_host_removed_1.fq \
+  -2 Result/${SAMPLE}_host_removed_2.fq \
+  -p minimap2 \
+  -t 32 \
+  --methods relative_abundance \
+> Result/MAG/${SAMPLE}_abundance.tsv
 
 #Step7:KEGG annotation
 #Prokka
